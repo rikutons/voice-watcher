e4c179735eac1cc250f78310f456d2b3a10af188
rikutons <rikutonsicloud@gmail.com>
2020-08-11 12:02:04 +0900

    Add pikapika effect of dyanamic effect

---
+++ Assets/Scripts/DynamicEffectMaker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicEffectMaker : MonoBehaviour
+{
+    [SerializeField]
+    GameObject pikapikaPerticle;
+    public void OnChangeText(string text){
+        if(text.Contains("ピカピカ")){
+            Instantiate(pikapikaPerticle);
+        }
+        if(text.Contains("ふわふわ")){
+            
+        }
+        if(text.Contains("かくかく")){
+            
+        }
+    }
+}
---
+++ Assets/Scripts/TextHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextHandler : MonoBehaviour
+{
+    [SerializeField]
+    private string text;
+    [SerializeField]
+    DynamicEffectMaker dynamicEffectMaker;
+    private void Start()
+    {
+        OnChangeText(text);
+    }
+
+    public void OnChangeText(string text)
+    {
+        dynamicEffectMaker.OnChangeText(text);
+    }
+}
