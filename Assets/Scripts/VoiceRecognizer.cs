5d96260e951f2b6fa44f8fb20bca5c159f89cfa3
rikutons <rikutonsicloud@gmail.com>
2020-10-01 23:29:07 +0900

    音声認識機能を追加

---
+++ Assets/Scripts/VoiceRecognizer.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrostweepGames.Plugins.GoogleCloud.SpeechRecognition
+{
+    public class VoiceRecognizer : MonoBehaviour
+    {
+        [SerializeField]
+        private GCSpeechRecognition speechRecognition;
+        [SerializeField]
+        private TextHandler textHandler;
+
+        void Start()
+        {
+            speechRecognition.RecognizeSuccessEvent += RecognizeSuccessEventHandler;
+
+            speechRecognition.FinishedRecordEvent += FinishedRecordEventHandler;
+            speechRecognition.StartedRecordEvent += StartedRecordEventHandler;
+            speechRecognition.RecordFailedEvent += RecordFailedEventHandler;
+
+            speechRecognition.BeginTalkigEvent += BeginTalkigEventHandler;
+            speechRecognition.EndTalkigEvent += EndTalkigEventHandler;
+
+            Debug.Log(speechRecognition.GetMicrophoneDevices()[1]);
+            speechRecognition.SetMicrophoneDevice(speechRecognition.GetMicrophoneDevices()[1]);
+            speechRecognition.StartRecord(true);
+        }
+
+        void OnDestroy()
+        {
+			speechRecognition.RecognizeSuccessEvent -= RecognizeSuccessEventHandler;
+
+			speechRecognition.FinishedRecordEvent -= FinishedRecordEventHandler;
+			speechRecognition.StartedRecordEvent -= StartedRecordEventHandler;
+			speechRecognition.RecordFailedEvent -= RecordFailedEventHandler;
+
+			speechRecognition.EndTalkigEvent -= EndTalkigEventHandler;
+        }
+
+        void RecognizeSuccessEventHandler(RecognitionResponse recognitionResponse){
+            Debug.Log(recognitionResponse);
+            if (recognitionResponse == null || recognitionResponse.results.Length == 0)
+            {
+                Debug.Log("Words not detected.");
+                return;
+            }
+            var words = recognitionResponse.results[0].alternatives[0].words;
+            Debug.Log(words);
+            if (words == null) return;
+            foreach (var item in words)
+            {
+                textHandler.OnChangeText(item.word);
+            }
+        }
+
+        private void FinishedRecordEventHandler(AudioClip clip, float[] raw)
+        {
+            Debug.Log("Finished Record");
+
+            RecognitionConfig config = RecognitionConfig.GetDefault();
+            config.languageCode = Enumerators.LanguageCode.ja_JP.Parse();
+            config.audioChannelCount = clip.channels;
+            // configure other parameters of the config if need
+
+            GeneralRecognitionRequest recognitionRequest = new GeneralRecognitionRequest()
+            {
+                audio = new RecognitionAudioContent()
+                {
+                    content = raw.ToBase64()
+                },
+                //audio = new RecognitionAudioUri() // for Google Cloud Storage object
+                //{
+                //	uri = "gs://bucketName/object_name"
+                //},
+                config = config
+            };
+
+            speechRecognition.Recognize(recognitionRequest);
+        }
+
+        private void StartedRecordEventHandler()
+        {
+            Debug.Log("Record Started");
+        }
+
+        private void RecordFailedEventHandler()
+        {
+            Debug.Log("Record Failed");
+        }
+
+        private void BeginTalkigEventHandler()
+        {
+            Debug.Log("Begin Talking");
+        }
+
+        private void EndTalkigEventHandler(AudioClip clip, float[] raw)
+        {
+            Debug.Log("End Talking");
+
+            FinishedRecordEventHandler(clip, raw);
+        }
+    }
+}
